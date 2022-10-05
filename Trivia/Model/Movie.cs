493044905a28c1592179e65d97dc15e6e839927c
fefegt <federic0duarte@hotmail.com.ar>
2022-10-04 22:02:47 -0300

    Trivia 1.0

--- Trivia/Model/Movie.cs
+++ Trivia/Model/Movie.cs
@@ -10,6 +10,8 @@ namespace Trivia.Model
     {
         public int Id { get; set; }
         public string Title { get; set; }
+
+        public string Emoji{ get; set; }
     }
 
     //public class Cast
--- Trivia/Services/MovieService.cs
+++ Trivia/Services/MovieService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using J3QQ4;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,16 +37,16 @@ namespace Trivia.Services
 
         private void PopulateMovies()
         {
-            Movies.Add(new Movie { Id = 1, Title = "The GodFather" });
-            Movies.Add(new Movie { Id = 2, Title = "The Dark Knight" });
-            Movies.Add(new Movie { Id = 3, Title = "The Godfather Part II" });
-            Movies.Add(new Movie { Id = 4, Title = "12 Angry Men" });
-            Movies.Add(new Movie { Id = 5, Title = "Schindler's List" });
-            Movies.Add(new Movie { Id = 6, Title = "The Lord of the Rings: The Return of the King" });
-            Movies.Add(new Movie { Id = 7, Title = "Pulp Fiction" });
-            Movies.Add(new Movie { Id = 8, Title = "The Good, the Bad and the Ugly" });
-            Movies.Add(new Movie { Id = 9, Title = "Forrest Gump" });
-            Movies.Add(new Movie { Id = 10, Title = "Fight Club" });
+            Movies.Add(new Movie { Id = 1, Title = "The GodFather" , Emoji = Emoji.Older_Man + Emoji.Gun + Emoji.Wine_Glass});
+            Movies.Add(new Movie { Id = 2, Title = "The Dark Knight" , Emoji = Emoji.Man + Emoji.Fist + Emoji.Night_With_Stars});
+            Movies.Add(new Movie { Id = 3, Title = "The Godfather Part II" , Emoji = Emoji.Gun + Emoji.Knife + Emoji.Horse + Emoji.Older_Man + Emoji.Two});
+            Movies.Add(new Movie { Id = 4, Title = "12 Angry Men" , Emoji = Emoji.One + Emoji.Angry + Emoji.Mens});
+            Movies.Add(new Movie { Id = 5, Title = "Schindler's List" , Emoji = Emoji.Girl + Emoji.Gun});
+            Movies.Add(new Movie { Id = 6, Title = "The Lord of the Rings: The Return of the King" , Emoji = Emoji.Older_Man + Emoji.Boy + Emoji.Ring + Emoji.Volcano});
+            Movies.Add(new Movie { Id = 7, Title = "Pulp Fiction" , Emoji = Emoji.Man + Emoji.Man + Emoji.Hamburger + Emoji.Video_Camera + Emoji.File_Folder});
+            Movies.Add(new Movie { Id = 8, Title = "The Good, the Bad and the Ugly" , Emoji = Emoji.Man + Emoji.Vs + Emoji.Man + Emoji.Vs + Emoji.Man});
+            Movies.Add(new Movie { Id = 9, Title = "Forrest Gump" , Emoji = Emoji.Runner + Emoji.Athletic_Shoe + Emoji.Man + Emoji.Wheelchair + Emoji.Sailboat + Emoji.Fried_Shrimp});
+            Movies.Add(new Movie { Id = 10, Title = "Fight Club" , Emoji = Emoji.Man + Emoji.Vs + Emoji.Man + Emoji.Secret + Emoji.Clubs });
         }
     }
 }
