c39923ba9305b9ca9d75dca7591e8a9e38f2e3cc
lyxetrader <80711138+lyxetrader@users.noreply.github.com>
2022-10-22 11:59:34 -0300

    aaa

--- Trivia/ViewModel/TriviaViewModel.cs
+++ Trivia/ViewModel/TriviaViewModel.cs
@@ -56,6 +56,9 @@ namespace Trivia.ViewModel
         [RelayCommand]
         private async Task GetMovies()
         {
+            AIService service = new AIService();
+            var result = await service.TestApi();
+            Shell.Current.DisplayAlert("t", result, "ok");
             try
             {
                 IsBusy = true;
